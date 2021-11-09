93f7cb24167f5a084d5d9d7d7486735f8770cb16
ilya-zhidkov <zidkov.ilja@gmail.com>
2021-11-09 23:50:39 +0100

    (api): Create patients endpoint
    We can now retrieve patients by sending GET request to our back-end.
    
    As follows:
    
    GET: /patients

---
+++ src/Nis.Api/Controllers/PatientsController.cs
@@ -0,0 +1,19 @@
+﻿using Nis.Core.Persistence;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nis.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PatientsController(DataContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> Index() => Ok(await _context.Patients.ToListAsync());
+    }
+}
