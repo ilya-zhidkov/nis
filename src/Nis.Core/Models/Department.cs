bc60eb253ffe61a2b1217a7661b4f2e586c25187
Jakub <jakubvydra.sw@gmail.com>
2022-03-16 00:32:20 +0100

    core: Add Department & Diagnose models and seeders

---
+++ src/Nis.Core/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using Nis.Core.Models.Diagnosis;
+using System.Collections.Generic;
+
+namespace Nis.Core.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Diagnose> Diagnoses{ get; set; } = new List<Diagnose>();
+    }
+}
