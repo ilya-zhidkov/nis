fb217a8a9406170da88b0e1000c09802ca87310d
Jakub Vydra <jakubvydra.sw@gmail.com>
2022-05-14 02:41:54 +0200

    core: Add domain models (Medical scale, Medical Scale Activity)

---
+++ server/src/Nis.Core/Models/Enums/MedicalScaleCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace Nis.Core.Models.Enums;
+
+public enum MedicalScaleCategory
+{
+    ADL = 1,
+    NutritionalStatusAssessmentScreening = 2,
+    NutritionalStatusAssessmentAdditionalExamination = 3,
+    RiskOfFall = 4,
+    RiskOfDecubitus = 5
+}
---
+++ server/src/Nis.Core/Models/MedicalScales/MedicalScaleActivity.cs
@@ -0,0 +1,7 @@
+﻿namespace Nis.Core.Models.MedicalScales;
+
+public class MedicalScaleActivity : BaseEntity
+{
+    public float Score { get; set; }
+    public string Name { get; set; }
+}
