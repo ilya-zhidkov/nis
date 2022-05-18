83641d80268d69a1be863b61ddc4c3b5f5ba6c40
PolMich <MichalPol@email.cz>
2022-05-18 15:48:41 +0200

    wpf: choose exam (placeholde)
    Choose exam view placeholder working

---
+++ client/Nis.WpfApp/ViewModels/ExamPickViewModel.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Caliburn.Micro;
+using Nis.WpfApp.Models;
+using Nis.Core.Persistence;
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nis.WpfApp.ViewModels
+{
+    internal class ExamPickViewModel : Screen
+    {
+
+        private readonly IEventAggregator _eventAggregator; 
+        private readonly IMapper _mapper;
+        private readonly DataContext _context;
+        private BindingList<Patient> _patients;
+
+        public BindingList<Patient> Patients
+        {
+            get => _patients;
+            set
+            {
+                _patients = value;
+                NotifyOfPropertyChange(() => Patients);
+            }
+        }
+
+        public ExamPickViewModel(IEventAggregator eventAggregator, IMapper mapper, DataContext context)
+        {
+            _eventAggregator = eventAggregator;
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task Activity() => await _eventAggregator.PublishOnUIThreadAsync("Exam");
+
+        protected override async void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+
+            await FetchPatientsAsync();
+        }
+
+        private async Task FetchPatientsAsync() => Patients = new BindingList<Patient>(
+            _mapper.Map<IList<Patient>>(await _context.Patients.ToListAsync())
+        );
+
+    }
+}
