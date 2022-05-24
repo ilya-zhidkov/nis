ca9c467459322faca119d43027ae8a0627dfd52d
Ilya Zhidkov <zidkov.ilja@gmail.com>
2022-05-24 19:28:49 +0200

    wpf: Retrieve medical scales for a risk of fall
    Wire up fall view with the actual risk of fall medical scales.

--- client/Nis.WpfApp/ViewModels/FallViewModel.cs
+++ client/Nis.WpfApp/ViewModels/FallViewModel.cs
@@ -1,21 +1,55 @@
-﻿using System.Windows;
+﻿using AutoMapper;
 using Caliburn.Micro;
+using System.Windows;
+using Nis.WpfApp.Models;
+using Nis.Core.Persistence;
+using Nis.Core.Models.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace Nis.WpfApp.ViewModels;
 
 public class FallViewModel : Screen
 {
-    private readonly IEventAggregator _eventAggregator;
+    private readonly IMapper _mapper;
+    private readonly DataContext _context;
+    private readonly IEventAggregator _aggregator;
+    private BindableCollection<MedicalScale> _scales;
 
-    public FallViewModel(IEventAggregator eventAggregator) => _eventAggregator = eventAggregator;
+    public BindableCollection<MedicalScale> Scales
+    {
+        get => _scales;
+        set
+        {
+            _scales = value;
+            NotifyOfPropertyChange(() => Scales);
+        }
+    }
 
-    public async Task Activity() => await _eventAggregator.PublishOnUIThreadAsync("Activity");
+    public FallViewModel(
+        IMapper mapper,
+        DataContext context,
+        IEventAggregator aggregator
+    )
+    {
+        _mapper = mapper;
+        _context = context;
+        _aggregator = aggregator;
+    }
 
-    public async Task Decubitus() => await _eventAggregator.PublishOnUIThreadAsync("Decubitus");
+    public async Task Activity() => await _aggregator.PublishOnUIThreadAsync("Activity");
 
-    public async Task Malnutrition() => await _eventAggregator.PublishOnUIThreadAsync("Malnutrition");
+    public async Task Decubitus() => await _aggregator.PublishOnUIThreadAsync("Decubitus");
 
-    public async Task Fall() => await _eventAggregator.PublishOnUIThreadAsync("Fall");
+    public async Task Malnutrition() => await _aggregator.PublishOnUIThreadAsync("Malnutrition");
+
+    public async Task Fall() => await _aggregator.PublishOnUIThreadAsync("Fall");
 
     public void Submit() => MessageBox.Show("Odesláno");
+
+    protected override async void OnViewLoaded(object view) => Scales = _mapper.Map<BindableCollection<MedicalScale>>(
+        await _context.MedicalScales
+            .Include(scale => scale.Activities)
+            .Where(scale => scale.ScaleCategory == MedicalScaleCategory.RiskOfFall)
+            .ToListAsync()
+    );
 }
