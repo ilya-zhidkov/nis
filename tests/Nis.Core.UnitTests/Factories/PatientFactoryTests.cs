3c67219dd902fbe7fb8b5469e926d208ffc4e030
Ilya Zhidkov <zidkov.ilja@gmail.com>
2022-06-07 00:31:55 +0200

    Reorganize folder structure
    As project grows it is normal for some parts to move.
    To keep things clean, we had to extract tests out of server.
    There are a few reasons for that:
    
    1. Ease of deployment
    - With tests separated from the main project it is easier to keep track of what goes into production.
    2. LIFT Principle
    - Instead of repeating tests/ inside of server/ and client/ we will keep folder structure flat.
    Not all projects will have complex test scenario anyway and adding an extra project seems redundant.

---
+++ tests/Nis.Core.UnitTests/Factories/PatientFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Nis.Core.Models;
+using FluentAssertions;
+using Nis.Core.Factories;
+
+namespace Nis.Core.UnitTests.Factories;
+
+public class PatientFactoryTests
+{
+    private readonly BaseFactory<Patient> _factory;
+
+    public PatientFactoryTests() => _factory = new PatientFactory();
+
+    [Fact]
+    public void it_should_be_an_instance_of_base_factory() => _factory.Should().BeAssignableTo<BaseFactory<Patient>>();
+
+    [Fact]
+    public void it_should_create_ten_patients() => _factory.Create(count: 10).Should().HaveCount(10);
+
+    [Fact]
+    public void it_should_throw_if_negative_count_is_passed() => ((Action)(() => _factory.Create(count: -1)))
+        .Should()
+        .Throw<ArgumentOutOfRangeException>();
+}
