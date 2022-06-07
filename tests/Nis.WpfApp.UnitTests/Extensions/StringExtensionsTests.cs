a4c812ede1664f8797756ef2d34faca570c721d7
Ilya Zhidkov <zidkov.ilja@gmail.com>
2022-06-07 13:49:16 +0200

    tests: Ensure that strings can be capitalized
    Long time no see.
    
    Let's kick tests off by ensuring that strings can be capitalized, eg.:
    First letter of a word is uppercase the rest is lowercase.

---
+++ tests/Nis.WpfApp.UnitTests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using FluentAssertions;
+using Nis.WpfApp.Extensions;
+
+namespace Nis.WpfApp.UnitTests.Extensions;
+
+public class StringExtensionsTests
+{
+    [Theory, InlineData(null), InlineData(""), InlineData(" ")]
+    public void it_should_return_empty_string_when_invalid_input_is_passed(string input) => input.Capitalize().Should().BeEmpty();
+
+    [Theory, InlineData("a", "A"), InlineData("lorem ipsum", "Lorem ipsum"), InlineData("LOREM IPSUM", "Lorem ipsum")]
+    public void it_should_return_capitalized_string(string input, string expected) => input.Capitalize().Should().Be(expected);
+}
