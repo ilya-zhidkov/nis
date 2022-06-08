53c78f37553fec7e39ed2f40b69a91fb8dc05703
Ilya Zhidkov <zidkov.ilja@gmail.com>
2022-06-08 19:45:08 +0200

    wpf: Handle enumerable edge cases
    Same old code but this time with extra protection.

--- client/Nis.WpfApp/Extensions/EnumerableExtensions.cs
+++ client/Nis.WpfApp/Extensions/EnumerableExtensions.cs
@@ -5,5 +5,9 @@ public static class EnumerableExtensions
     public static IDictionary<TKey, TValue> Merge<TKey, TValue>(
         this IDictionary<TKey, TValue> dictionary,
         IDictionary<TKey, TValue> with
-    ) => dictionary.Union(with).ToDictionary(pair => pair.Key, pair => pair.Value);
+    ) => dictionary switch
+    {
+        null when with is not null => new Dictionary<TKey, TValue>().Union(with).ToDictionary(pair => pair.Key, pair => pair.Value),
+        _ => with is null ? dictionary : dictionary.Union(with).ToDictionary(pair => pair.Key, pair => pair.Value)
+    };
 }
