5ad4453b5e622278416e79ea897976fd248f9c7c
Ilya Zhidkov <zidkov.ilja@gmail.com>
2022-06-08 20:02:53 +0200

    tests: Ensure that enumerables can be merged
    A whole set of unit tests to make sure that we can perform union operation upon our own collections.
    
    As you can tell, we are not being as harsh with nulls.
    Instead of throwing exceptions we are trying the best we can to perform a union.

---
+++ tests/Nis.WpfApp.UnitTests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using FluentAssertions;
+using Nis.WpfApp.Extensions;
+
+namespace Nis.WpfApp.UnitTests.Extensions;
+
+public class EnumerableExtensionsTests
+{
+    [Fact]
+    public void it_should_return_null_when_merging_nulls() => ((Dictionary<int, string>)null).Merge(null).Should().BeNull();
+
+    [Fact]
+    public void it_should_merge_empty_dictionaries() => new Dictionary<int, string>().Merge(new Dictionary<int, string>()).Should().BeEmpty();
+
+    [Fact]
+    public void it_should_merge_distinct_dictionaries() => new Dictionary<int, string> { { 1, "a" } }
+        .Merge(new Dictionary<int, string> { { 2, "b" } })
+        .Should().BeEquivalentTo(new Dictionary<int, string> { { 1, "a" }, { 2, "b" } });
+
+    [Fact]
+    public void it_should_not_merge_duplicates() => new Dictionary<int, string> { { 1, "a" } }
+        .Merge(new Dictionary<int, string> { { 1, "a" } })
+        .Should().BeEquivalentTo(new Dictionary<int, string> { { 1, "a" } });
+
+    [Fact]
+    public void it_should_merge_first_if_second_is_null() => new Dictionary<int, string> { { 1, "a" } }
+        .Merge(null)
+        .Should().BeEquivalentTo(new Dictionary<int, string> { { 1, "a" } });
+
+    [Fact]
+    public void it_should_merge_second_if_first_is_null() => ((Dictionary<int, string>)null)
+        .Merge(new Dictionary<int, string> { { 2, "b" } })
+        .Should().BeEquivalentTo(new Dictionary<int, string> { { 2, "b" } });
+}
