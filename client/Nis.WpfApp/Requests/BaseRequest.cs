50ac57ccac9e45faf69f73bef10d127b61ee9c83
Ilya Zhidkov <zidkov.ilja@gmail.com>
2022-06-11 22:18:55 +0200

    wpf: Condense HTTP calls
    Up until now the way we were calling protected HTTP endpoints was by using HTTP extensions.
    Which was very awkward and unintuitive. Not anymore!
    
    By merging HTTP extensions into BaseRequest the API now looks consistent.
    
    Also, a minor issue I overlooked at first was the declaration of HttpClient.
    We used to allocate a brand new HttpClient for every request which is an obvious performance penalty.
    
    By marking it as static we are reusing the same HTTP instance across all requests.

--- client/Nis.WpfApp/Requests/BaseRequest.cs
+++ client/Nis.WpfApp/Requests/BaseRequest.cs
@@ -2,12 +2,13 @@
 using System.Net.Http.Json;
 using Nis.WpfApp.Extensions;
 using System.Net.Http.Headers;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Nis.WpfApp.Requests;
 
 public abstract class BaseRequest
 {
-    private readonly HttpClient _http;
+    private static HttpClient _http;
     protected static readonly Dictionary<string, string> Headers = new();
     protected readonly string Endpoint = App.Configuration["Api:Endpoint"];
 
@@ -15,31 +16,41 @@ public abstract class BaseRequest
     {
         _http = new HttpClient { BaseAddress = new Uri(Endpoint) };
         _http.DefaultRequestHeaders.Accept.Clear();
-        _http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        _http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(Application.Json));
         _http.DefaultRequestHeaders.ToDictionary(header => header.Key, header => header.Value.First()).Merge(Headers);
     }
 
-    protected async Task<TResult> GetAsync<TResult>(string uri, IDictionary<string, string> headers = null)
+    protected static async Task<TResult> GetAsync<TResult>(string uri, IDictionary<string, string> headers = null)
     {
-        using var response = headers is null ? await _http.GetAsync(uri) : await _http.GetWithHeadersAsync(uri, headers);
+        var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+        if (headers is not null)
+            foreach (var (key, value) in headers)
+                request.Headers.Add(key, value);
+
+        var response = await _http.SendAsync(request);
 
         return response.IsSuccessStatusCode
             ? await response.Content.ReadFromJsonAsync<TResult>()
             : throw new Exception(response.ReasonPhrase);
     }
 
-    protected async Task<TResult> PostAsync<TResult>(string uri, HttpContent content, IDictionary<string, string> headers = null)
+    protected static async Task<TResult> PostAsync<TResult>(string uri, HttpContent content, IDictionary<string, string> headers = null)
     {
-        using var response = headers is null
-            ? await _http.PostAsync(uri, content)
-            : await _http.PostWithHeadersAsync(uri, content, headers);
+        var request = new HttpRequestMessage(HttpMethod.Post, uri) { Content = content };
+
+        if (headers is not null)
+            foreach (var (key, value) in headers)
+                request.Headers.Add(key, value);
+
+        var response = await _http.SendAsync(request);
 
         return response.IsSuccessStatusCode
             ? await response.Content.ReadFromJsonAsync<TResult>()
             : throw new Exception(response.ReasonPhrase);
     }
 
-    protected void Authenticate(string token)
+    protected static void Authenticate(string token)
     {
         if (!_http.DefaultRequestHeaders.Contains("Authorization"))
             _http.DefaultRequestHeaders.Add("Authorization", token);
