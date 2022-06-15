1a250b815d45e566eab107908f89f8caae73e718
Ilya Zhidkov <zidkov.ilja@gmail.com>
2022-06-16 00:21:14 +0200

    tests: Setup integration tests
    That one caught me off guard.
    
    Initially, the plan was to mock HttpClient but the boilerplate code wasn't worth it.

---
+++ tests/Nis.WpfApp.IntegrationTests/Requests/SignInRequestTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Bogus.DataSets;
+using Caliburn.Micro;
+using FluentAssertions;
+using Nis.WpfApp.Requests;
+using Nis.WpfApp.Configuration;
+
+namespace Nis.WpfApp.IntegrationTests.Requests;
+
+public class SignInRequestTests : BaseIntegrationTest
+{
+    private readonly SignInRequest _request;
+
+    public SignInRequestTests() => _request = new SignInRequest(new SimpleContainer());
+
+    [Fact]
+    public async Task it_should_return_authentication_response_if_credentials_are_valid() => (await _request
+        .SignInAsync(Settings.Configuration["Credentials:Username"], Settings.Configuration["Credentials:Password"]))
+        .Should().BeOfType<SignInRequest.AuthenticationResponse>();
+
+    [Fact]
+    public async Task it_should_return_null_if_credentials_are_not_valid() => (await _request
+        .SignInAsync(new Internet().UserName(), new Internet().Password()))
+        .Should().BeNull();
+}
